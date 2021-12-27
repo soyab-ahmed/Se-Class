d294fd3adbf945f248e5d2c8fe976449c8a5aad6
[soyab-ahmed] <[sahmed182033@bscse.uiu.ac.bd]>
2021-12-27 14:40:25 +0600

    class work

---
+++ 2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main (){
+
+    printf("Today is Monday");
+
+    return 0;
+}
---
+++ 3.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main (){
+
+    printf("This is Software lab");
+
+    return 0;
+}
